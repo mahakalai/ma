bb84d827d00bf89bc44ed4eb07ba9f48c631fd15
mahakalai <46440625+mahakalai@users.noreply.github.com>
2019-02-14 22:02:46 +0530

    Create pset1-5.py

---
+++ pset1-5.py
@@ -0,0 +1,5 @@
+z=input()
+q=['I','II','III','IV','V','VI','VII','VIII','IX','X','XI','XII','XIII','XIV','XV','XVI','XVII','XVIII','XIX','XXX']
+for i in range(0,len(r)):
+	if z==q[i]:
+		print(i+1)
