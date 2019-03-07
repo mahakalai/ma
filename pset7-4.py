e3cbf36e4dbfddb1b68f16ca70370f4a92f3e0ba
mahakalai <46440625+mahakalai@users.noreply.github.com>
2019-03-07 21:30:18 +0530

    Create pset7-4.py

---
+++ pset7-4.py
@@ -0,0 +1,10 @@
+n,k=map(int,input().split())
+l=[int(x) for x in input().split()]
+l.sort()
+l1=[]
+for i in range(0,len(l)):
+     if l[i]<k:
+        l1.append(l[i])
+for i in range(0,len(l1)-1):
+     print(l1[i],end=" ")
+print(l1[-1])
